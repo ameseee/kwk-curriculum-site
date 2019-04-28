5c4083c2ba270db926e3ca2df6c2596124fa1542
Amy Holt <amy.elizabeth.holt@gmail.com>
2019-04-28 14:26:45 -0600

    Add drop down menus for each camp

---
+++ public/js/drop-downs.js
@@ -0,0 +1,14 @@
+$('.web-app-drop-down').on('mouseover', function() {
+  $('.web-app-options').css('display', 'inline-block');
+    $('.mobile-app-options').css('display', 'none');
+});
+
+$('.mobile-app-drop-down').on('mouseover', function() {
+  $('.mobile-app-options').css('display', 'inline-block');
+  $('.web-app-options').css('display', 'none');
+});
+
+$('img').on('mouseover', function() {
+  $('.mobile-app-options').css('display', 'none');
+  $('.web-app-options').css('display', 'none');
+});
