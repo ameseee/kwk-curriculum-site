8fdf6bf1a05c4d07c17ce4952b0e189aa9a31d8d
Amy Holt <amy.elizabeth.holt@gmail.com>
2019-04-29 11:17:18 -0600

    Add logs to active links to debug them in prod

--- public/js/active-links.js
+++ public/js/active-links.js
@@ -1,15 +1,23 @@
 $('.curriculum-links').each(function(idx, link) {
   var currentPath = window.location.pathname;
   var linkPath = $(link).attr('href');
+  console.log("linkPath:", linkPath);
 
   var currentCamp = currentPath.split('/')[1];
+  console.log("currentPath:", currentPath);
+
   var campPath = `/${currentCamp}/`
+  console.log("campPath:", campPath);
 
   if (linkPath.includes(currentCamp)) {
+    console.log("inside of IF, and the link is:", link);
+    console.log("inside of IF, and the linkPath is:", linkPath);
+    console.log("inside of IF, and the currentCamp is:", currentCamp);
     $(link).toggleClass('active-link');
   }
 
   if(currentPath === "/") {
+    console.log("HOME:", currentPath);
     $(link).removeClass('active-link');
   }
 });
