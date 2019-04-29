6a944b549ab3caef20a2c4a964917d264a7e32af
Amy Holt <amy.elizabeth.holt@gmail.com>
2019-04-29 10:59:37 -0600

    Hide drops downs on hover of content

--- public/js/drop-downs.js
+++ public/js/drop-downs.js
@@ -8,12 +8,7 @@ $('.mobile-app-drop-down').on('mouseover', function() {
   $('.web-app-options').css('display', 'none');
 });
 
-$('img').on('mouseover', function() {
-  $('.mobile-app-options').css('display', 'none');
-  $('.web-app-options').css('display', 'none');
-});
-
-$('h1').on('mouseover', function() {
+$('.content').on('mouseover', function() {
   $('.mobile-app-options').css('display', 'none');
   $('.web-app-options').css('display', 'none');
 });
