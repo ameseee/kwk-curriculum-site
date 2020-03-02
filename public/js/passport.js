df5132554c2a494ba76e04a9d9173b345f1d672e
Amy Holt <amy.elizabeth.holt@gmail.com>
2020-03-01 20:19:56 -0700

    Implement passport

---
+++ public/js/passport.js
@@ -0,0 +1,75 @@
+let relativeURL = window.location.pathname;
+const logOutForm = document.querySelector('.logout-form');
+const logInForm = document.querySelector('.login-form');
+
+function checkLocation() {
+  const check = localStorage.getItem('loggedIn') || 'false';
+
+  if (relativeURL === '/login/' && check === 'true') {
+    showLinks();
+    displayLogout();
+  } else if (relativeURL === '/login/' && check === 'false') {
+    displayLogin();
+    var loginBtn = document.querySelector("#login-submit");
+    loginBtn.addEventListener('click', logIn);
+  } else if (relativeURL !== '/login/' && check === 'true') {
+    showLinks();
+  }
+}
+
+checkLocation();
+
+function displayLogin() {
+  logOutForm.style.display = 'none';
+  logInForm.style.display = 'block';
+}
+
+function displayLogout() {
+  logOutForm.style.display = 'block';
+  logInForm.style.display = 'none';
+  logOutForm.addEventListener('submit', logOut);
+}
+
+function logIn(event) {
+  event.preventDefault();
+
+  let login = document.getElementById("login").value.trim();
+  let password = document.getElementById("password").value.trim();
+
+  if (checkLogin(login) && checkPassword(password)) {
+    localStorage.setItem('loggedIn', 'true');
+    window.location.replace("/");
+    showLinks();
+  } else {
+    alert("Sorry! Try again.")
+  }
+}
+
+function showLinks() {
+  var links = document.querySelectorAll(".navbar-navigation--links .curriculum-links");
+  for (var i = 0; i < 2; i++) {
+    links[i].style.display = 'block';
+  }
+}
+
+function hideLinks() {
+  var links = document.querySelectorAll(".navbar-navigation--links .curriculum-links");
+  for (var i = 0; i < 2; i++) {
+    links[i].style.display = 'none';
+  }
+}
+
+function checkLogin(login) {
+  return login === "kwk";
+}
+
+function checkPassword(password) {
+  return password === "boss";
+}
+
+function logOut(event) {
+  event.preventDefault();
+  localStorage.setItem('loggedIn', 'false');
+  window.location.replace("/login/");
+  hideLinks();
+}
