8373a9ccbf245b37473eb4a2d7ee375b11657ac9
Amy Holt <amy.elizabeth.holt@gmail.com>
2020-03-10 12:31:16 -0600

    Update Emojional project

--- public/js/passport.js
+++ public/js/passport.js
@@ -1,6 +1,4 @@
 let relativeURL = window.location.pathname;
-const logOutForm = document.querySelector('.logout-form');
-const logInForm = document.querySelector('.login-form');
 
 function checkLocation() {
   const check = localStorage.getItem('loggedIn') || 'false';
@@ -14,67 +12,72 @@ function checkLocation() {
     loginBtn.addEventListener('click', logIn);
   } else if (relativeURL !== '/kwk-curriculum-site/login/' && check === 'true') {
     showLinks();
-  } else if (relativeURL !== '/kwk-curriculum-site/login/' && check === 'false') {
-    window.location.replace('/kwk-curriculum-site/login/');
+  // } else if (relativeURL !== '/kwk-curriculum-site/login/' && check === 'false') {
+  //   window.location.replace('/kwk-curriculum-site/login/');
   }
 
 }
 
 checkLocation();
 
-function displayLogin() {
-  logOutForm.style.display = 'none';
-  logInForm.style.display = 'block';
-  //change to login h1
-}
+document.addEventListener("DOMContentLoaded", function() {
+  const logOutForm = document.querySelector('.logout-form');
+  const logInForm = document.querySelector('.login-form');
 
-function displayLogout() {
-  logOutForm.style.display = 'block';
-  logInForm.style.display = 'none';
-  logOutForm.addEventListener('submit', logOut);
-  //change to logout h1
-}
 
-function logIn(event) {
-  event.preventDefault();
-
-  let login = document.getElementById("login").value.trim();
-  let password = document.getElementById("password").value.trim();
-
-  if (checkLogin(login) && checkPassword(password)) {
-    localStorage.setItem('loggedIn', 'true');
-    window.location.replace("/kwk-curriculum-site/");
-    showLinks();
-  } else {
-    alert("Sorry! Try again.")
+  function displayLogin() {
+    logOutForm.style.display = 'none';
+    logInForm.style.display = 'block';
   }
-}
 
-function showLinks() {
-  var links = document.querySelectorAll(".navbar-navigation--links .curriculum-links");
-  for (var i = 0; i < 2; i++) {
-    links[i].style.display = 'block';
+  function displayLogout() {
+    logOutForm.style.display = 'block';
+    logInForm.style.display = 'none';
+    logOutForm.addEventListener('submit', logOut);
   }
-}
 
-function hideLinks() {
-  var links = document.querySelectorAll(".navbar-navigation--links .curriculum-links");
-  for (var i = 0; i < 2; i++) {
-    links[i].style.display = 'none';
+  function logIn(event) {
+    event.preventDefault();
+
+    let login = document.getElementById("login").value.trim();
+    let password = document.getElementById("password").value.trim();
+
+    if (checkLogin(login) && checkPassword(password)) {
+      localStorage.setItem('loggedIn', 'true');
+      window.location.replace("/kwk-curriculum-site/");
+      showLinks();
+    } else {
+      alert("Sorry! Try again.")
+    }
   }
-}
 
-function checkLogin(login) {
-  return login === "kwk";
-}
+  function showLinks() {
+    var links = document.querySelectorAll(".navbar-navigation--links .curriculum-links");
+    for (var i = 0; i < 2; i++) {
+      links[i].style.display = 'block';
+    }
+  }
 
-function checkPassword(password) {
-  return password === "boss";
-}
+  function hideLinks() {
+    var links = document.querySelectorAll(".navbar-navigation--links .curriculum-links");
+    for (var i = 0; i < 2; i++) {
+      links[i].style.display = 'none';
+    }
+  }
 
-function logOut(event) {
-  event.preventDefault();
-  localStorage.setItem('loggedIn', 'false');
-  window.location.replace("/kwk-curriculum-site/login/");
-  hideLinks();
-}
+  function checkLogin(login) {
+    return login === "kwk";
+  }
+
+  function checkPassword(password) {
+    return password === "boss";
+  }
+
+  function logOut(event) {
+    event.preventDefault();
+    localStorage.setItem('loggedIn', 'false');
+    window.location.replace("/kwk-curriculum-site/login/");
+    hideLinks();
+  }
+
+});
